0d2372d2a8a50c031070c2bf1de59c1f7865463e
Daniel Pasillas Rdz <b.pasillas.4@gmail.com>
2018-05-22 13:42:08 -0500

    Se agregó el ViewModel para controlar el Slider de imágenes.

---
+++ LARWebSite/Models/SliderModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using LARWebSite.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LARWebSite.Models
+{
+    public class SliderModel
+    {
+        public int itHasLink { get; set; }
+        public string Descripcion { get; set; }
+        public string Imagen { get; set; }
+
+        public SliderModel(slider _slider)
+        {
+            this.itHasLink = _slider.hasLink;
+            this.Descripcion = _slider.description;
+            this.Imagen = _slider.Image;
+        }
+    }
+}
---
+++ LARWebSite/ViewModels/IndexViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using LARWebSite.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LARWebSite.ViewModels
+{
+
+    /*
+     *  In this view model we will save the set of models that will let index module to work..
+     *  i.e. The Slider Model, the category model, the brands model and so on.
+     *  Everything will be fill up automatically.
+     */
+    public class IndexViewModel
+    {
+        public IEnumerable<SliderModel> Carousel { get; set; }
+    }
+}
