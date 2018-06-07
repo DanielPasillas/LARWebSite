13f69e05a09f5c99b69b60c0a6f1fc03dfa33d35
Daniel Pasillas Rdz <b.pasillas.4@gmail.com>
2018-06-06 21:06:41 -0500

    Commit for saving changes.

--- LARWebSite/Assets/js/red/products/visualizer.detail.js
+++ LARWebSite/Assets/js/red/products/visualizer.detail.js
@@ -1,11 +1,1 @@
-﻿/*
- * Assis Inc Design and Programming S.A. de C.V.
- */
-
-(function () {
-    window.onscroll = function (ev) {
-        if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight+600)) {
-            console.log("Example of alert..");
-        }
-    };
-})();
+﻿
